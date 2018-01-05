17301f1557581b7fa2de1ad7150e590ad4b417fd
MrazTevin <millatevin@gmail.com>
2018-01-05 16:17:46 +0300

    modified my fourth quote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
     new Quote(1,'Nine tenths of wisdom consists of being wise in time.'),
     new Quote(2,'Anything more than the true would be too much'),
     new Quote(3,'Its much better to do it in a way that nobody knows about it'),
-    new Quote(4,`Movies have takes.But plays are like life you don't really get takes`)
+    new Quote(4,`Movies have takes.But plays are like life you don't really get takes`),
+    
   ]
  }
