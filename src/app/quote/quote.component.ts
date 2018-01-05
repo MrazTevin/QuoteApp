f8733f570d66f45842b0dca8caa5e2b9108498bc
MrazTevin <millatevin@gmail.com>
2018-01-05 15:02:55 +0300

    deleted everything and started afresh

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,15 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Quotes} from 'quotes';
+
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
 export class QuoteComponent implements OnInit {
-  quotes = [
-    new Quote(1, 'It may have been in pieces, but I gave you the best of me.'),
-    new Quote(1, 'Everything should be made as simple as possible, but not simple'),
-  ]
 
   constructor() { }
 
