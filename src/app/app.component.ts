8130627c6d6d4882f26248295b4f9338a2444e79
MrazTevin <millatevin@gmail.com>
2018-01-05 16:57:22 +0300

    addecd my 9th and 10th quote

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,5 +16,7 @@ export class AppComponent {
     new Quote(6,'The more we do the more we can do;the more busy we are, the more leisure we have.'),
     new Quote(7,'The more you learn the more you earn.'),
     new Quote(8,'One who sees inaction in action and action in inaction is intelligent among men.'),
+    new Quote(9,'Our acts mark or mar us, we are the children of our own deeds.'),
+    new Quote(10,`Words may show a man's wit but actions his meaning.`),
   ]
  }
